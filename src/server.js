72885ed44708c10b9b13817872d44f47e0a6cd61
omar <omarqais225@gmail.com>
2025-10-30 23:42:06 +0200

    create cors for my app on react native

--- src/server.js
+++ src/server.js
@@ -5,7 +5,19 @@ import purchaseRoute from './routes/purchases.route.js';
 import transactionRoute from './routes/transactions.route.js';
 import productRoute from './routes/products.route.js';
 import express, { response, request } from 'express';
+import cors from 'cors';
 const app = express();
+const allowedOrigins = [process.env.EXPO_APP, process.env.EXPO_URL];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 if (process.env.NODE_ENV === 'production') job.start();
 app.use(express.json());
 app.get(`/api/tracker`, (request, response) => {
